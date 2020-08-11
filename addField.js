54ac53fe5184f93aa0fdcbb52d523739932dba22
Stephanie Maso <69222712+StephanieMaso@users.noreply.github.com>
2020-08-10 22:58:35 -0300

    Create addField.js

---
+++ addField.js
@@ -0,0 +1,24 @@
+//Procurar o botão
+document.querySelector("#add-time")
+//Quando clicar no botão
+.addEventListener('click', cloneField)
+//Executar uma ação
+function cloneField () {
+    //Duplicar os campos (que campo?)
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) //boolean: true ou false
+    
+    //Pegar os campos (que campos?)
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    //Para cada campo, limpá-lo
+    fields.forEach(function(field){
+        //pegar o field do momento e limpa ele
+        field.value = ""   
+     })
+
+    //Colocar na página (onde?)
+    document.querySelector('#schedule-itens').appendChild(newFieldContainer)
+
+}
+
+
